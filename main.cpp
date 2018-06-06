299cb8622851651d4244a326aee29e024d4b1bbb
arizkiredita <arizkiredita970@gmail.com>
2018-06-06 12:57:59 +0700

    batang

--- main.cpp
+++ main.cpp
@@ -6,16 +6,11 @@
 
 #include <stdlib.h>
 
-static int slices = 16;
-static int stacks = 16;
-
 /* GLUT callback Handlers */
 
 static void resize(int width, int height)
 {
     const float ar = (float) width / (float) height;
-
-    glViewport(0, 0, width, height);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     glFrustum(-ar, ar, -1.0, 1.0, 2.0, 100.0);
@@ -27,14 +22,13 @@ static void resize(int width, int height)
 static void display(void)
 {
     const double t = glutGet(GLUT_ELAPSED_TIME) / 1000.0;
-    const double a = t*90.0;
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glColor3d(1,0,0);
 
     glPushMatrix();
-    glColor3d(0.5,0.5,0.5);
-        glTranslated(0,0,-6);
+    glColor3d(0.8,0.8,0.8);
+        glTranslated(0,0,-8);
         glRotated(60,1,0,0);
         glScalef(0.4,0.4,3);
         glutSolidCube(1);
@@ -51,19 +45,6 @@ static void key(unsigned char key, int x, int y)
         case 'q':
             exit(0);
             break;
-
-        case '+':
-            slices++;
-            stacks++;
-            break;
-
-        case '-':
-            if (slices>3 && stacks>3)
-            {
-                slices--;
-                stacks--;
-            }
-            break;
     }
 
     glutPostRedisplay();
@@ -73,17 +54,6 @@ static void idle(void)
 {
     glutPostRedisplay();
 }
-
-const GLfloat light_ambient[]  = { 0.0f, 0.0f, 0.0f, 1.0f };
-const GLfloat light_diffuse[]  = { 1.0f, 1.0f, 1.0f, 1.0f };
-const GLfloat light_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
-const GLfloat light_position[] = { 2.0f, 5.0f, 5.0f, 0.0f };
-
-const GLfloat mat_ambient[]    = { 0.7f, 0.7f, 0.7f, 1.0f };
-const GLfloat mat_diffuse[]    = { 0.8f, 0.8f, 0.8f, 1.0f };
-const GLfloat mat_specular[]   = { 1.0f, 1.0f, 1.0f, 1.0f };
-const GLfloat high_shininess[] = { 100.0f };
-
 /* Program entry point */
 
 int main(int argc, char *argv[])
@@ -111,16 +81,6 @@ int main(int argc, char *argv[])
     glEnable(GL_NORMALIZE);
     glEnable(GL_COLOR_MATERIAL);
     glEnable(GL_LIGHTING);
-
-    glLightfv(GL_LIGHT0, GL_AMBIENT,  light_ambient);
-    glLightfv(GL_LIGHT0, GL_DIFFUSE,  light_diffuse);
-    glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
-    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
-
-    glMaterialfv(GL_FRONT, GL_AMBIENT,   mat_ambient);
-    glMaterialfv(GL_FRONT, GL_DIFFUSE,   mat_diffuse);
-    glMaterialfv(GL_FRONT, GL_SPECULAR,  mat_specular);
-    glMaterialfv(GL_FRONT, GL_SHININESS, high_shininess);
 
     glutMainLoop();
 
