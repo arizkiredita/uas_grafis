e453b267c5557ba48c22bcba1d9792a4d5929406
fansdnc <151111061@mhs.stiki.ac.id>
2018-07-02 13:51:17 +0700

    kepala_fan

--- src/org/yourorghere/Objek.java
+++ src/org/yourorghere/Objek.java
@@ -4,14 +4,14 @@ import javax.media.opengl.glu.GLU;
 import javax.media.opengl.glu.GLUquadric;
 public class Objek {
     
-    static void Tiang(GL gl){
-        float BODY_LENGTH=3.0f;
-        float BODY_RADIUS=0.4f;
+   static void Tiang(GL gl){
+        float BODY_LENGTH=6f;
+        float BODY_RADIUS=1f;
         GLU glu=new GLU();
         int SLICES=30;
         int STACKS=30;
         double clip_plane1[]={0.0, 0.0, -1.0, 0.0};
-        gl.glColor3f(0,1,0);
+        gl.glColor3f(0,1,1);
         gl.glClipPlane(GL.GL_CLIP_PLANE1, clip_plane1,0);
         gl.glEnable(GL.GL_CLIP_PLANE1);
         GLUquadric qd=glu.gluNewQuadric();
@@ -29,8 +29,41 @@ public class Objek {
         glu.gluDisk(q, 0.0f, BODY_RADIUS, SLICES, STACKS);
         glu.gluDisk(q, 0.0f, BODY_RADIUS, SLICES, STACKS);
     }
-    
-    static void balingbaling(GL gl){
+   //kepala
+   static void setengahlingkaran(GL gl){
+        float BODY_RADIUS=0.2f;
+        int SLICES=10;
+        int STACKS=10;
+        GLU glu=new GLU();
+        gl.glColor3f(1,0,0);
+        GLUquadric q=glu.gluNewQuadric();
+         glu.gluDisk(q, 0.0f, 1.0f, SLICES, STACKS);
+        double clip_plane1[]={0.0,0.0,-1.0,0.0};
+        gl.glClipPlane(GL.GL_CLIP_PLANE1,clip_plane1,0);
+        gl.glEnable(GL.GL_CLIP_PLANE1);
+        // drawing a sphere
+        GLUquadric qd=glu.gluNewQuadric();
+        glu.gluSphere(qd,1f,20,20);
+        glu.gluDeleteQuadric(qd);
+        gl.glDisable(GL.GL_CLIP_PLANE1);
+    }
+   static void tabung(GL gl){
+float BODY_LENGTH=2.0f;
+float BODY_RADIUS=1.0f;
+int SLICES=30;
+int STACKS=30; 
+GLU glu=new GLU();
+gl.glColor3f(1,1,0);
+GLUquadric q=glu.gluNewQuadric();
+glu.gluCylinder(q, BODY_RADIUS, BODY_RADIUS, 
+BODY_LENGTH, SLICES, STACKS);
+glu.gluDisk(q, 0.0f, BODY_RADIUS, SLICES, STACKS);
+//lingkaran untuk tutup atas
+gl.glTranslatef(0.0f, 0.0f, BODY_LENGTH);
+glu.gluDisk(q, 0.0f, BODY_RADIUS, SLICES, STACKS);
+//lingkaran untuk tutup bawah
+}
+   static void balingbaling(GL gl){
         gl.glBegin(GL.GL_TRIANGLES);
         gl.glColor3f(1.0f, 0.0f,0.0f); // Set the current drawing color to red
             gl.glVertex3f(0f, 0f, 0.0f);   // Top
@@ -44,14 +77,25 @@ public class Objek {
             gl.glVertex3f(0.75f, 1f, 0.0f);  // Bottom Right
         gl.glEnd();
     }
-    static void Tombol(GL gl){
-        float BODY_RADIUS=0.1f;
+//   static void bawah(GL gl){
+//       gl.glBegin(GL.GL_LINES);
+//       gl.glVertex3f(-4.0f, 0f, 0f);
+//       gl.glVertex3f(4f, 0f, 0f);
+//       gl.glVertex3f(4.0f, 4f, 0f);
+//              gl.glVertex3f(4f, 0f, 0f);
+//
+//       gl.glVertex3f(-4.0f, -4f, 0f);
+//       gl.glVertex3f(-4.0f, 0f, 0f);
+//              gl.glVertex3f(4f, 0f, 0f);
+//
+//   }
+   static void Tombol(GL gl){
+        float BODY_RADIUS=0.2f;
         int SLICES=50;
         int STACKS=50;
         GLU glu=new GLU();
         GLUquadric q=glu.gluNewQuadric();
         gl.glColor3f(0,0,0);
         glu.gluSphere(q, BODY_RADIUS, SLICES, STACKS);
-            }
-
+    }
 }
